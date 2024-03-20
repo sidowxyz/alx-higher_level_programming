b9f9c3a3eba5d9c188005e49c2209f94ff08d1ec
sidowxyz <sidowxyz@yahoo.com>
2024-03-20 13:37:22 -0700

    Add a function that deletes a key in a dictionary.

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to delete a key in a dictionary
-
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
